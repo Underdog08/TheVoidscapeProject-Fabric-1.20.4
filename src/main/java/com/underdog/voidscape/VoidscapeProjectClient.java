b66fbc7e48cd50e660d275298ecc56b07f9112a0
Underdog08 <148916684+Underdog08@users.noreply.github.com>
2024-08-30 13:20:56 -0500

    Initial commit

---
+++ src/main/java/com/underdog/voidscape/VoidscapeProjectClient.java
@@ -0,0 +1,10 @@
+package com.underdog.voidscape;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VoidscapeProjectClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
